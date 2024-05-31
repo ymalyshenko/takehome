a390389d2f00ca0504af7a8c5984f1ec044160ef
prnvkbr <pranavkuber@gmail.com>
2024-05-31 13:28:57 -0400

    added a sample component for page #1

---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+
+export default function Main() {
+    return (
+        <div>
+        <Typography variant="h1">Question 1</Typography>
+        </div>
+    );
+}
