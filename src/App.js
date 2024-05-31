86877b3bc4569a0d52d9709952f6ba63d715d108
prnvkbr <pranavkuber@gmail.com>
2024-05-31 13:27:48 -0400

    added theme with a sample baseline color scheme, CustomRoutes to add additional routes/screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {createTheme, ScopedCssBaseline, ThemeProvider} from "@mui/material";
+import CustomRoutes from "./CustomRoutes";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Lato',
+      'sans-serif'
+    ].join(',')
+  },
+  palette: {
+    primary: {
+      main: '#5f6368',
+    },
+    secondary: {
+      main: '#1E4181',
+    },
+    piction: {
+      background: '#eaeaea',
+      text: '#6c6c72',
+      white: '#fff'
+    },
+    grayTones: {
+      main: '#636363',
+      light: '#eeeeee',
+      lighter: '#f5f5f5',
+      ultralight: '#fafafa',
+    }
+  },
+  spacing: 8,
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <ScopedCssBaseline>
+          <CustomRoutes />
+        </ScopedCssBaseline>
+    </ThemeProvider>
   );
 }
 
