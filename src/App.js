290102662b1bf135d8905ea6599975df553d193b
prnvkbr <pranavkuber@gmail.com>
2024-05-31 13:42:06 -0400

    updated theme

--- src/App.js
+++ src/App.js
@@ -4,29 +4,26 @@ import CustomRoutes from "./CustomRoutes";
 
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      'Lato',
-      'sans-serif'
-    ].join(',')
+    fontFamily: ['Lato', 'sans-serif'].join(','),
   },
   palette: {
     primary: {
-      main: '#5f6368',
+      main: '#fff',
     },
     secondary: {
-      main: '#1E4181',
+      main: '#484848',
     },
     piction: {
       background: '#eaeaea',
       text: '#6c6c72',
-      white: '#fff'
+      white: '#fff',
     },
     grayTones: {
       main: '#636363',
       light: '#eeeeee',
       lighter: '#f5f5f5',
       ultralight: '#fafafa',
-    }
+    },
   },
   spacing: 8,
 });
