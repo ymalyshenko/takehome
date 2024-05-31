094e62f2869ff7cceb0db2edfa66903263edbc50
prnvkbr <pranavkuber@gmail.com>
2024-05-31 13:42:48 -0400

    added a custom app bar component to be included on all pages

---
+++ src/components/CustomAppBar.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import {Typography} from "@mui/material";
+
+
+export default function CustomAppBar() {
+
+    return (
+        <AppBar elevation={0} position="static">
+            <Container maxWidth="xl" sx={{paddingTop: 5, display: 'flex', justifyContent: 'center'}}>
+                <Typography variant="h5">New assessment</Typography>
+            </Container>
+        </AppBar>
+    );
+}
